ba10d9e3996054ccd6e029779f47edec51e88dd6
G00410024@atu.ie <g00410024@atu.ie>
2023-10-11 14:35:21 +0100

    Project Setup

---
+++ src/main/java/ie/atu/listeningmicroservice/ListeningMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.listeningmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeningMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListeningMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/listeningmicroservice/ListeningMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.listeningmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListeningMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
