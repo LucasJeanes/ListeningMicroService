17701c4b7bd6d6f1526b6f3f835bea57cb398cf7
G00410024@atu.ie <g00410024@atu.ie>
2023-10-18 12:50:17 +0100

    Added new microservice to pass inputted data from microservice 1(User controller). RegistrationController takes inputted data from port 8081, passes userDetails to microservice 3 to generate completed String.

--- src/main/java/ie/atu/listeningmicroservice/ListeningMicroServiceApplication.java
+++ src/main/java/ie/atu/listeningmicroservice/ListeningMicroServiceApplication.java
@@ -2,8 +2,9 @@ package ie.atu.listeningmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
-
+@EnableFeignClients
 @SpringBootApplication
 public class ListeningMicroServiceApplication {
 
